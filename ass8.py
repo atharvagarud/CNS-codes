455cd0e8d4eff231b0948feb1a93a2a1e195733c
atharvagarud <101793633+atharvagarud@users.noreply.github.com>
2023-10-31 17:52:27 +0530

    Update ass8.py

--- ass8.py
+++ ass8.py
@@ -1,38 +1,32 @@
-# Caesar Cipher encryption function
-def encrypt(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            shifted_char = chr((ord(char) + shift - 65) % 26 + 65) if char.isupper() else chr((ord(char) + shift - 97) % 26 + 97)
-            encrypted_text += shifted_char
-        else:
-            encrypted_text += char
-    return encrypted_text
+from cryptography.fernet import Fernet
 
-# Caesar Cipher decryption function
-def decrypt(text, shift):
-    decrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            shifted_char = chr((ord(char) - shift - 65) % 26 + 65) if char.isupper() else chr((ord(char) - shift - 97) % 26 + 97)
-            decrypted_text += shifted_char
-        else:
-            decrypted_text += char
-    return decrypted_text
+# Generate a random key for encryption and decryption
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
-# Get user input for encrypted image and shift value
-encrypted_image_path = input("Enter the path to the encrypted image file: ")
-shift = int(input("Enter the shift value used for encryption (an integer between 1 and 25): "))
+# Take input image file path from the user
+input_image_path = input("Enter the path of the image file you want to transmit: ")
 
-# Read encrypted image data as binary
-with open(encrypted_image_path, "rb") as file:
-    encrypted_image_data = file.read()
+# Read the image file as binary data
+with open(input_image_path, "rb") as file:
+    image_data = file.read()
 
-# Decrypt encrypted image data
-decrypted_image_data = decrypt(encrypted_image_data.decode("latin-1"), shift)
+# Encrypt the image data
+encrypted_data = cipher_suite.encrypt(image_data)
 
-# Save decrypted image data to a new file
-decrypted_image_path = "decrypted_image.png"
-with open(decrypted_image_path, "wb") as file:
-    file.write(decrypted_image_data.encode("latin-1"))
-print(f"Image decrypted and saved as {decrypted_image_path}")
+# Save the encrypted data to a new file
+encrypted_file_path = "encrypted_image.bin"
+with open(encrypted_file_path, "wb") as file:
+    file.write(encrypted_data)
+
+print("Image encrypted and saved as 'encrypted_image.bin'. Transmit this file securely.")
+
+# Decrypt the image data
+decrypted_data = cipher_suite.decrypt(encrypted_data)
+
+# Save the decrypted data to a new file
+decrypted_file_path = "decrypted_image.jpg"
+with open(decrypted_file_path, "wb") as file:
+    file.write(decrypted_data)
+
+print("Image decrypted and saved as 'decrypted_image.jpg'.")
