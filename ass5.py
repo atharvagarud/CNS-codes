92f28096f9bff8cf564067a0e466cef1ffac22c5
atharvagarud <101793633+atharvagarud@users.noreply.github.com>
2023-11-22 23:34:16 +0530

    Update ass5.py

--- ass5.py
+++ ass5.py
@@ -1,6 +1,15 @@
 import hashlib
-
-sha1_hash = hashlib.sha1(message.encode()).hexdigest()
-message = input("Enter the message to be hashed: ")
-print("Hash value generated: ", sha1_hash)
-print("SHA-1 Hash of the message:", hash_value)
+# Function to calculate the SHA-1 hash of a message
+def sha1(message):
+    sha1_hash = hashlib.sha1(message.encode()).hexdigest()
+    return sha1_hash
+# Main function
+def main():
+    # User input for the message
+    message = input("Enter the message to be hashed: ")
+    # Calculate SHA-1 hash
+    hash_value = sha1(message)
+    # Display the calculated SHA-1 hash
+    print("SHA-1 Hash of the message:", hash_value)
+if __name__ == "__main__":
+    main()
